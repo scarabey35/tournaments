597122dc25fb79e2c89633ab7112735d3cc64dca
scarabey35 <den.zzz168@gmail.com>
2026-02-19 09:31:01 +0200

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,15 @@
+from enum import Enum
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app.extensions import db
+
+
+class Role(str, Enum):
+    organizer = "organizer"
+    participant = "participant"
+    jury = "jury"
+
+
+
 
