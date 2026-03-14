eea8cdccd4bf92407f2e2b89212b2a129686bd21
camorezka <sadovoyura777333arbuz@gmail.com>
2026-03-14 19:56:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 import os
 from app.routes.landing import landing_bp
+from app.routes.admin import admin
+
+
+
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 template_dir = os.path.join(base_dir, "app", "templates")
 static_dir = os.path.join(base_dir, "app", "static")
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-
+app.register_blueprint(admin)
 app.register_blueprint(landing_bp)
 
 if __name__ == "__main__":
