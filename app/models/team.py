b0219f1a9b38be9cdb5caff1ea6bb03717c83a70
camorezka <sadovoyura777333arbuz@gmail.com>
2026-03-14 19:55:17 +0300

    Update team.py

--- app/models/team.py
+++ app/models/team.py
@@ -4,4 +4,7 @@ class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
-    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'))
+    tournament_id = db.Column(
+        db.Integer,
+        db.ForeignKey("tournament.id")
+    )
