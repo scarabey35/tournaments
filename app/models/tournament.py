eb01f78dac6ebad69e4c028a57ac438966910ed6
camorezka <sadovoyura777333arbuz@gmail.com>
2026-03-14 19:54:57 +0300

    Update tournament.py

--- app/models/tournament.py
+++ app/models/tournament.py
@@ -5,5 +5,3 @@ class Tournament(db.Model):
     name = db.Column(db.String(100))
     deadline = db.Column(db.String(100))
     description = db.Column(db.Text)
-
-    teams = db.relationship('Team', backref='tournament', lazy=True)
