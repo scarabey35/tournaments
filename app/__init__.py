00d46497ac7708f4208e2015e512c7027c617dbe
115Base <134048148+115Base@users.noreply.github.com>
2026-04-09 08:27:10 +0300

    Integrate Flask-Login for user management
    Added Flask-Login integration for user authentication.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
+from flask_login import LoginManager
 from .models import db
+from .extension import migrate
+
+login_manager = LoginManager()
+login_manager.login_view = 'user.login'
+login_manager.login_message = 'Будь ласка, увійдіть для доступу до цієї сторінки.'
+login_manager.login_message_category = "info"
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../instance/app.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
     db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    @login_manager.user_loader
+    def load_user(user_id):
+        from .models.user import User
+        return User.query.get(int(user_id))
 
     return app
