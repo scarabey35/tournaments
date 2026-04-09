40a150eb2fb6fc0af4b2d91d110c86e811ce91a5
115Base <134048148+115Base@users.noreply.github.com>
2026-04-09 08:32:31 +0300

    Fixed app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask
 from flask_session import Session 
 from app.models import db
 from app.extension import migrate
-
 from app.routes.landing import landing_bp
 from app.routes.admin import admin
 from app.routes.tournaments import tournaments_bp
 from app.routes.user import user_bp
+
 
 def create_app():
     app = Flask(__name__, 
@@ -21,14 +21,13 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    app.register_blueprint(landing_bp)
+    app.register_blueprint(landing_db)
     app.register_blueprint(admin, url_prefix="/admin")
     app.register_blueprint(tournaments_bp, url_prefix="/tournaments")
     app.register_blueprint(user_bp)
 
     with app.app_context():
         db.create_all()
-
     return app
 
 app = create_app()
