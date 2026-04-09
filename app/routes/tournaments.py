f1229612b3dea4c8ea5269d167af9c595d950b19
scarabey35 <den.zzz168@gmail.com>
2026-04-09 19:17:46 +0300

    Update tournaments.py

--- app/routes/tournaments.py
+++ app/routes/tournaments.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from app.models import Tournament
 
 tournaments_bp = Blueprint("tournaments", __name__)
 
+
 @tournaments_bp.route("/")
 def tournaments_list():
-    tournaments = Tournament.query.all()
+    tournaments = Tournament.query.order_by(Tournament.created_at.desc()).all()
     return render_template("tournaments.html", tournaments=tournaments)
 
 
@@ -18,10 +20,10 @@ def tournament_detail(tournament_id):
 @tournaments_bp.route("/<int:tournament_id>/leaderboard")
 def leaderboard(tournament_id):
     tournament = Tournament.query.get_or_404(tournament_id)
-
-    # поки просто передаємо турнір
     return render_template("leaderboard.html", tournament=tournament)
+
 
 @tournaments_bp.route("/leaderboard")
 def leaderboard_global():
-    return render_template("leaderboard.html")
+    tournaments = Tournament.query.filter_by(status="finished").all()
+    return render_template("leaderboard.html", tournaments=tournaments)
