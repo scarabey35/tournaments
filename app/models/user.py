7c669fdafd1da054fb598855c1a81417c8173fe8
scarabey35 <den.zzz168@gmail.com>
2026-04-09 19:07:26 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from flask_login import UserMixin
 from . import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
