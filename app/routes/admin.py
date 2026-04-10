26a49cdcb14ae522ba134fc6152261f19596f03a
115Base <134048148+115Base@users.noreply.github.com>
2026-04-10 18:58:27 +0300

    Fix formatting issue in admin.py import statement

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,4 +1,4 @@
-﻿from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
 
 from app.decorators import roles_required
