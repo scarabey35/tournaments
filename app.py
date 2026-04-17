39e0843024115f77cc384e59e7c1c54698754fec
115Base <134048148+115Base@users.noreply.github.com>
2026-04-17 17:09:18 +0300

    Add Flask-Migrate import to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from app.models import db
 from app.extension import migrate
 from app.routes.landing import landing_bp
