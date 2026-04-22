593b1f046a20a1075cc258bd69d2a0d86708b968
kyrapyto <nikitawork466@gmail.com>
2026-04-22 17:43:53 +0300

    add import for flashed messages

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template
+from flask import Blueprint, request, redirect, url_for, flash, render_template, get_flashed_messages
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models import db, User, Team
